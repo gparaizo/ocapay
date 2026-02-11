a5c806ec508ee0f4ab8f5c79d6a55596edd8f716
Gabriel Paraizo <36803487+gparaizo@users.noreply.github.com>
2026-02-11 02:10:00 -0300

    chore: Add apikey
    In order to create a new account

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/gparaizo/ocapay
 
 go 1.25.7
+
+require github.com/google/uuid v1.6.0
--- internal/domain/account.go
+++ internal/domain/account.go
@@ -1,6 +1,12 @@
 package domain
 
-import "time"
+import (
+	"crypto/rand"
+	"encoding/hex"
+	"time"
+
+	"github.com/google/uuid"
+)
 
 type Account struct {
 	ID        string
@@ -10,4 +16,22 @@ type Account struct {
 	Balance   float64
 	CreatedAt time.Time
 	UpdatedAt time.Time
+}
+
+func GenerateAPIKey() string {
+	key := make([]byte, 16)
+	rand.Read(key)
+	return hex.EncodeToString(key)
+}
+
+func NewAccount(name string, email string) *Account {
+	account := &Account{
+		ID:        uuid.New().String(),
+		Name:      name,
+		Email:     email,
+		Balance:   0,
+		APIKey:    GenerateAPIKey(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
 }
